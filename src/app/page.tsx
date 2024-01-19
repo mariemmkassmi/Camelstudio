fd6c77509b5a70698ab66a60604231c79c727138
mariemmkassmi <mariem.mkassmi@bearstudio.fr>
2024-01-19 22:57:27 +0100

    fix the texte

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -334,7 +334,7 @@ export default function Hero () {
               href='mailto:contact@camelstudiotunisie.com'
               className='rounded-md bg-blue-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
             >
-              Démarrons l'aventure ensemble!
+              Démarrons l&rsquo;aventure ensemble!
             </a>
           </p>
         </div>
